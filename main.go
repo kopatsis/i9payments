4465fdc1f1547c35e4836d312a7c7210ea504391
James Kopatsis <jkopatsis@gmail.com>
2024-05-21 23:17:25 -0700

    first

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"i9pay/platform"
+	"i9pay/platform/login"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		if os.Getenv("APP_ENV") != "production" {
+			log.Fatalf("Failed to load the env vars: %v", err)
+		}
+	}
+
+	auth := login.InitFirebase()
+
+	rtr := platform.New(auth)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4040"
+	}
+
+	if err := http.ListenAndServe(":"+port, rtr); err != nil {
+		log.Fatalf("There was an error with the http server: %v", err)
+	}
+}
---
+++ platform/router.go
@@ -0,0 +1,44 @@
+package platform
+
+import (
+	"net/http"
+
+	"i9pay/platform/login"
+	"i9pay/platform/middleware"
+
+	"firebase.google.com/go/auth"
+	"github.com/gin-gonic/gin"
+)
+
+func New(auth *auth.Client) *gin.Engine {
+	router := gin.Default()
+
+	router.Use(middleware.CORSMiddleware())
+	router.Static("/static", "./static")
+
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+
+		uid := "None"
+
+		tokenID, err := login.ExtractUIDFromSession(c, auth)
+		if err == nil {
+			uid = tokenID
+		}
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Title":  "Public Page",
+			"Header": "Welcome to Public Page",
+			"UID":    uid,
+			"Year":   2024,
+		})
+	})
+
+	router.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.tmpl", nil)
+	})
+
+	router.POST("/verifyToken", login.VerifyToken(auth))
+
+	return router
+}
