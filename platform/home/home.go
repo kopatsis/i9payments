2e8198b114b5dd31310db63e039dfb08e8aea30c
James Kopatsis <jkopatsis@gmail.com>
2024-05-23 22:39:45 -0700

    admin panel basics

---
+++ platform/home/home.go
@@ -0,0 +1,42 @@
+package home
+
+import (
+	"context"
+	"i9pay/platform/login"
+	"i9pay/platform/multipass"
+	"net/http"
+
+	"firebase.google.com/go/auth"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func AdminPanel(auth *auth.Client, database *mongo.Database) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uid, err := login.ExtractUIDFromSession(c, auth)
+		if err != nil {
+			c.Redirect(http.StatusFound, "/login")
+			return
+		}
+
+		userRecord, err := auth.GetUser(context.Background(), uid)
+		if err != nil {
+			c.Redirect(http.StatusFound, "/login")
+			return
+		}
+
+		email := userRecord.Email
+		user, err := multipass.UserFromUID(uid, database)
+		if err != nil {
+			c.Redirect(http.StatusFound, "/login")
+			return
+		}
+
+		c.HTML(200, "admin.tmpl", gin.H{
+			"ID":     user.ID.Hex(),
+			"Email":  email,
+			"Paying": user.Paying,
+			"Name":   user.Name,
+		})
+	}
+}
