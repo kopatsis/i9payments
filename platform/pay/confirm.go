943d6b32c385a83914bf4032c023dc69de9f8775
James Kopatsis <jkopatsis@gmail.com>
2024-06-08 14:03:31 -0700

    actual fail webhook

--- platform/pay/confirm.go
+++ platform/pay/confirm.go
@@ -116,3 +116,87 @@ func WebhookConfirm(auth *auth.Client, database *mongo.Database) gin.HandlerFunc
 		}
 	}
 }
+
+func WebhookFail(auth *auth.Client, database *mongo.Database) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		const MaxBodyBytes = int64(65536)
+		c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, MaxBodyBytes)
+
+		payload, err := c.GetRawData()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error reading request body"})
+			return
+		}
+
+		endpointSecret := os.Getenv("ENDPOINT_SECR")
+
+		event, err := webhook.ConstructEvent(payload, c.GetHeader("Stripe-Signature"), endpointSecret)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error verifying webhook signature"})
+			return
+		}
+
+		if event.Type == "invoice.payment_failed" {
+			var invoice stripe.Invoice
+			if err := json.Unmarshal(event.Data.Raw, &invoice); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Error parsing webhook JSON"})
+				return
+			}
+
+			subscription := invoice.Subscription
+			customerID := invoice.Customer.ID
+
+			customerReal, err := customer.Get(customerID, nil)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Error customer doesn't exist"})
+				return
+			}
+
+			paymentIntent, _ := paymentintent.Get(invoice.PaymentIntent.ID, nil)
+			userId := paymentIntent.Metadata["userId"]
+
+			if userId == "" {
+				userId = customerReal.Metadata["userId"]
+			}
+
+			if userId == "" {
+				var userPayment db.UserPayment
+				filter := bson.M{"subid": subscription.ID}
+				err := database.Collection("userpayment").FindOne(context.TODO(), filter).Decode(&userPayment)
+				if err != nil {
+					c.JSON(http.StatusBadRequest, gin.H{"error": "Error user id doesn't exist in either metafield or userpayment collection"})
+					return
+				}
+				userId = userPayment.UserMongoID
+			}
+
+			objID, err := primitive.ObjectIDFromHex(userId)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Error converting user id to ObjectID"})
+				return
+			}
+			var user db.User
+			userFilter := bson.M{"_id": objID}
+			err = database.Collection("user").FindOne(context.TODO(), userFilter).Decode(&user)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Error retrieving user from user collection"})
+				return
+			}
+
+			userRecord, err := auth.GetUser(context.Background(), user.Username)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Error retrieving user from firebase"})
+				return
+			}
+
+			if err := emails.SendFailureNotification(userRecord.Email, user.Name); err != nil {
+				log.Printf("Error in emailing user: %s; %s", userRecord.Email, err.Error())
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to email user for payment failure"})
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{"status": "success"})
+		}
+	}
+}
